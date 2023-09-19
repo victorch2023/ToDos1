5b48db37447c4b37597658f6f24067b24b9897cd
victorch2023 <chowwha@gmail.com>
2023-09-19 14:23:25 -0500

    yes

--- index.js
+++ index.js
@@ -22,8 +22,26 @@ function App(){
         }
     ]);
 
+    const [value, setValue] = React.useState('')
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        const newsTodos = [...todos, {text:value, isCompleted:false}];
+        setTodos(newsTodos);
+        setValue('');
+    }
+
     return (<>
             {todos.map((todo,i) => <div className="todo" key={i}>{todo.text}</div>)}
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    className="input"
+                    value={value}
+                    placeholder="Add Todo ..."
+                    onChange={e => setValue(e.target.value)}
+                />
+            </form>
         </>);
 }
 
